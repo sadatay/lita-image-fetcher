5ce6dcaf7f312eba71966d1ba4903c7dbd6c7641
Joe LaSala <jdlasala@gmail.com>
2015-12-04 07:23:59 +0000

    Tidying up unused locale stuff.

--- lib/lita-image-fetcher.rb
+++ lib/lita-image-fetcher.rb
@@ -1,12 +1,4 @@
 require "lita"
 
-Lita.load_locales Dir[File.expand_path(
-  File.join("..", "..", "locales", "*.yml"), __FILE__
-)]
-
 require "lita/handlers/image_fetcher"
 
-Lita::Handlers::ImageFetcher.template_root File.expand_path(
-  File.join("..", "..", "templates"),
- __FILE__
-)
