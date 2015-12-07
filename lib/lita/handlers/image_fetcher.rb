f435c039a7b2e079feeefcad2c16a3ec9577313d
Joseph LaSala <joe.lasala@whitehatsec.com>
2015-12-07 00:28:37 -0600

    Cleaning up some logic

--- lib/lita/handlers/image_fetcher.rb
+++ lib/lita/handlers/image_fetcher.rb
@@ -1,15 +1,14 @@
 module Lita
   module Handlers
     class ImageFetcher < Handler
-      config :pixabay_key, types: [String], default: "1733162-8502bd72ccb45f2043fbe2208"
-      config :google_cse_key, types: [String], default: "AIzaSyDCJf_JUCIdnT_4d-bbBEJhoske5wB_ZYk"
-      config :google_cse_id, types: [String], default: "005286386203807831899:6uwitwgujla"
+      config :pixabay_key, types: [String]
+      config :google_cse_key, types: [String]
+      config :google_cse_id, types: [String]
       config :google_cse_safe_search, types: [Symbol], default: :high
 
       route(/(?:image|img)(?:\s+me)? (.+)/i, :fetch, command: true, help: {
         "image QUERY" => "Displays an image matching the query."
       })
-
 
       # TODO: gracefully handle sources that aren't configured
       def sources
@@ -24,24 +23,15 @@ module Lita
       end
 
       def fetch(response)
-        reply = "No images found from sources: #{sources.keys.join(', ')}"
-        reply_image = nil
-        reply_source = nil
-
         sources.each do |source_name, source_object|
           image_url = source_object.fetch(response)
           if validate(image_url)
-            reply_image = image_url
-            reply_source = source_name
-            break
+            response.reply "#{image_url} (From #{source_name})"
+            return
           end
         end
 
-        if reply_image
-          reply = "#{reply_image} (From #{reply_source})"
-        end
-
-        response.reply reply
+        response.reply "No images found from sources: #{sources.keys.join(', ')}"
       end
 
       def validate(url)
